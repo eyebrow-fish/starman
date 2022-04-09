b8d452544ad3c846aa6eae2cca68bea3e29ff6ee
Alexander Johnston <Aliics@hotmail.com>
2022-04-09 12:14:27 +1200

    theme setup

---
+++ src/nav-list/NavList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list">
+    <div
+      v-for="(item, i) in value"
+      :key="i"
+    >{{ item.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavList',
+  props: {
+    value: Array,
+  },
+}
+</script>
+
+<style scoped>
+.list {
+  display: flex;
+  flex-direction: column;
+
+  background-color: var(--bg-color);
+  color: var(--fg-color);
+}
+</style>
