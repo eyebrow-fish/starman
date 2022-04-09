e31861dfd71763d6abae52b169d06b9a08c50847
Alexander Johnston <Aliics@hotmail.com>
2022-04-09 19:36:39 +1200

    resizable list

--- src/query-list/QueryList.vue
+++ src/query-list/QueryList.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="list">
-    <input type="text" v-model="search">
-    <div
-      v-for="(item, i) in items"
-      :key="i"
-    >
-      <QueryItem :value="item"/>
+  <div class="main">
+    <div class="list" :style="{width: `${listWidth}px`}">
+      <div class="search-area">
+        <input class="search" type="text" v-model="search" placeholder="example.org">
+      </div>
+      <div v-for="(item, i) in items" :key="i">
+        <QueryItem :value="item"/>
+      </div>
     </div>
+    <div
+      class="resize-anchor"
+      :class="{resizing: resizing}"
+      @mousedown="startResize"
+    ></div>
   </div>
 </template>
 
@@ -23,27 +29,75 @@ export default {
   data() {
     return {
       search: '',
+      listWidth: 160,
+      resizing: false,
     }
+  },
+  mounted() {
+    document.addEventListener('mouseup', this.endResizeListener)
+  },
+  unmounted() {
+    document.removeEventListener('mouseup', this.endResizeListener)
   },
   computed: {
     items() {
       return this.value.filter(i =>
-        searchMatch(i.method, this.search) || searchMatch(i.name, this.search)
+        searchMatch(i.method, this.search) || searchMatch(i.name, this.search),
       )
+    },
+  },
+  methods: {
+    startResize() {
+      this.resizing = true
+      document.body.style.cursor = 'col-resize'
+      document.addEventListener('mousemove', this.resizeListener)
+    },
+    endResizeListener() {
+      if (!this.resizing) return
+      this.resizing = false
+      document.body.style.cursor = 'default'
+      document.removeEventListener('mousemove', this.resizeListener)
+    },
+    resizeListener(e) {
+      this.listWidth += e.movementX
     },
   },
 }
 </script>
 
 <style scoped>
+.main {
+  display: flex;
+  overflow-x: hidden;
+}
+
+.resize-anchor {
+  border-left: solid 1px var(--border-color);
+  cursor: col-resize;
+  border-right: solid 4px transparent;
+}
+
+.resize-anchor:hover, .resize-anchor.resizing {
+  border-left-color: var(--accent-color);
+}
+
 .list {
   display: flex;
   flex-direction: column;
-
+  overflow-y: auto;
+  min-width: 160px;
   border-right: solid 1px var(--border-color);
-  width: 10em;
 
   background-color: var(--bg-color);
   color: var(--fg-color);
+}
+
+.search-area {
+  padding: 0.5em;
+  border-bottom: solid 1px var(--border-color);
+}
+
+.search-area .search {
+  width: 100%;
 }
 </style>
